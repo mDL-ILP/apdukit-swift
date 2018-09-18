b6a74d8c8f49b03ae137caeba8542efcd4ae99fd
Iain Munro <munro.iain@ul.com>
2018-09-18 13:16:52 +0200

    Made start on the BLE transport layer with bluetoothkit

---
+++ Example/ReaderViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ReaderViewController.swift
+//  Example
+//
+//  Created by Iain Munro on 18/09/2018.
+//  Copyright © 2018 UL-TS. All rights reserved.
+//
+
+import UIKit
+
+class ReaderViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ apduKit/interpreter/DeviceType.swift
@@ -0,0 +1,9 @@
+//
+//  DeviceType.swift
+//  apduKit
+//
+//  Created by Iain Munro on 17/09/2018.
+//  Copyright © 2018 UL-TS. All rights reserved.
+//
+
+import Foundation
